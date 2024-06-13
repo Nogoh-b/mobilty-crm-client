22a0d623bd5c8ab0ccfc6a1d37ae4240eb80f8ea
Nogoh Brice <nogohbrice@gmail.com>
2024-06-13 17:28:22 +0100

    LogicOnBoadrding

--- app/(pages)/(auth)/register/page.tsx
+++ app/(pages)/(auth)/register/page.tsx
@@ -1,28 +1,4 @@
 "use client"
-import Image from "next/image";
-import Input from "../../../component/ui/Input";
-import Textarea from "../../../component/ui/Textarea";
-import UploadInput from "../../../component/ui/UploadInput";
-import Alert from "../../../component/ui/Alert";
-import Avatar from "../../../component/ui/Avatar";
-import AvatarList from "../../../component/ui/AvatarList";
-import Badge from "../../../component/ui/Badge";
-import ButtonGroup from "../../../component/ui/ButtonGroup";
-import Divider from "../../../component/ui/Divider";
-import RangeSlider from "../../../component/ui/RangeSlider";
-import Spinner from "../../../component/ui/spinner";
-import Button from "../../../component/ui/Button";
-import Stepper from "../../../component/ui/Stepper";
-import Social from "../../../component/ui/Social";
-import Treeview from "../../../component/ui/Treeview";
-import Timeline from "../../../component/Timeline";
-import Card from "../../../component/ui/Card";
-import Chart from "../../../component/ui/Chart";
-import PieChart from "../../../component/ui/PieChart";
-import DoughnutChart from "../../../component/ui/DoughnutChart";
-import PolarAreaChart from "../../../component/ui/PolarAreaChart";
-import { DoughnutChartData, PieChartData, PolarAreaChartData } from "../../../constants/AppConstants";
-import Dropdown from "../../../component/ui/Dropdown";
 
 export default function Register() {
 
@@ -51,7 +27,7 @@ export default function Register() {
 							</span>
 							<input type="text" className="form-control" placeholder="Name"/>
 						</div>
-						<div class="mb-3 input-group input-group-icon">
+						<div className="mb-3 input-group input-group-icon">
 							<span className="input-group-text">
 								<div className="input-icon">
 									<svg width="24" height="24" viewBox="0 0 24 24" fill="none">
--- app/(pages)/onboarding/page.tsx
+++ app/(pages)/onboarding/page.tsx
@@ -1,84 +1,104 @@
-"use client"
+'use client';
+
+import { useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import 'swiper/swiper-bundle.min.css';
 
 export default function Onboarding() {
+  useEffect(() => {
+    import('swiper/swiper-bundle.min.css');
+  }, []);
 
- 
   return (
     <>
       <div className="vh-100 w-100 bg-primary d-flex align-items-center justify-content-center align-content-center">
-        <div className="">
-          <img src="/assets/images/logo.png" width={100} height={100} />
+        <div>
+          <img src="/assets/images/logo.png" width={100} height={100} alt="Logo" />
         </div>
         <div className="mb-3">
-						<div className="swiper-container get-started">
-							<div className="swiper-wrapper">
-								<div className="swiper-slide">
-									<div className="started">
-										<h1 className="title">Admission scolaire</h1>
-										<p>Trouvez, postulez, et gérez vos dossiers en toute simplicité, garantissant ainsi un avenir académique prometteur. Nous sommes partenaires de plusieurs groupes d’écoles à l’étranger</p>
-									</div>
-								</div>
-								<div className="swiper-slide">
-									<div className="started">
-										<h1>Accompagnement personnalisé</h1>
-										<p> Recherche de logement, préparation aux entretiens, formalités VISA ou assurances, nous simplifions chaque étape pour vous permettre de vous concentrer sur l'essentiel : votre réussite.</p>
-									</div>
-								</div>
-								<div className="swiper-slide">
-									<div className="started">
-										<h1>Caution bancaire(AVI)</h1>
-										<p>Nous travaillons avec des partenaires bancaires locaux que nous connaissons bien et avec lesquels nous avons bâti des bases solides de confiance. Dés à présent  obtenez votre AVI avec 0 frais de service</p>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="swiper-btn">
-							<div className="swiper-pagination style-1 flex-1"></div>
-						</div>
-					</div>
-
-
-        
+          <Swiper
+            modules={[Pagination]}
+            pagination={{ clickable: true }}
+            className="swiper-container get-started"
+          >
+            <SwiperSlide className="swiper-slide">
+              <div className="started">
+                <h1 className="title">Admission scolaire</h1>
+                <p>
+                  Trouvez, postulez, et gérez vos dossiers en toute simplicité,
+                  garantissant ainsi un avenir académique prometteur. Nous sommes
+                  partenaires de plusieurs groupes d’écoles à l’étranger.
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="swiper-slide">
+              <div className="started">
+                <h1>Accompagnement personnalisé</h1>
+                <p>
+                  Recherche de logement, préparation aux entretiens, formalités VISA ou
+                  assurances, nous simplifions chaque étape pour vous permettre de vous
+                  concentrer sur l'essentiel : votre réussite.
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="swiper-slide">
+              <div className="started">
+                <h1>Caution bancaire(AVI)</h1>
+                <p>
+                  Nous travaillons avec des partenaires bancaires locaux que nous
+                  connaissons bien et avec lesquels nous avons bâti des bases solides de
+                  confiance. Dés à présent obtenez votre AVI avec 0 frais de service.
+                </p>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+          <div className="swiper-btn">
+            <div className="swiper-pagination style-1 flex-1"></div>
+          </div>
+        </div>
       </div>
-	<div className="content-body d-none">
-		<div className="container vh-100">
-			<div className="welcome-area">
-				<div className="bg-image bg-image-overlay" ></div>
-				<div className="join-area">
-					<div className="mb-3">
-						<div className="swiper-container get-started">
-							<div className="swiper-wrapper">
-								<div className="swiper-slide">
-									<div className="started">
-										<h1 className="title">Hello, And Welcome to Soziety.</h1>
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-									</div>
-								</div>
-								<div className="swiper-slide">
-									<div className="started">
-										<h1>People's First Prefrence Is Soziety</h1>
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-									</div>
-								</div>
-								<div className="swiper-slide">
-									<div className="started">
-										<h1>Expand Your Network With Soziety</h1>
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="swiper-btn">
-							<div className="swiper-pagination style-1 flex-1"></div>
-						</div>
-					</div>
-					<a href="register.html" className="btn btn-primary btn-block mb-3">CREATE ACCOUNT</a>
-					<a href="login.html" className="btn btn-light btn-block mb-3">SIGN IN</a>
-					<a href="forgot-password.html" className="text-light text-center d-block">Forgot your account?</a>
-				</div>
-			</div>
-		</div>
-	</div>
+      <div className="content-body d-none">
+        <div className="container vh-100">
+          <div className="welcome-area">
+            <div className="bg-image bg-image-overlay"></div>
+            <div className="join-area">
+              <div className="mb-3">
+                <Swiper
+                  modules={[Pagination]}
+                  pagination={{ clickable: true }}
+                  className="swiper-container get-started"
+                >
+                  <SwiperSlide className="swiper-slide">
+                    <div className="started">
+                      <h1 className="title">Hello, And Welcome to Soziety.</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                  </SwiperSlide>
+                  <SwiperSlide className="swiper-slide">
+                    <div className="started">
+                      <h1>People's First Preference Is Soziety</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                  </SwiperSlide>
+                  <SwiperSlide className="swiper-slide">
+                    <div className="started">
+                      <h1>Expand Your Network With Soziety</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                  </SwiperSlide>
+                </Swiper>
+                <div className="swiper-btn">
+                  <div className="swiper-pagination style-1 flex-1"></div>
+                </div>
+              </div>
+              <a href="register.html" className="btn btn-primary btn-block mb-3">CREATE ACCOUNT</a>
+              <a href="login.html" className="btn btn-light btn-block mb-3">SIGN IN</a>
+              <a href="forgot-password.html" className="text-light text-center d-block">Forgot your account?</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
