6f784c1bab00e445ba940443486d764777d9eecf
Nogoh Brice <nogohbrice@gmail.com>
2024-10-22 17:58:25 +0200

    first commit

--- app/component/ui/DoughnutChart.tsx
+++ app/component/ui/DoughnutChart.tsx
@@ -1,14 +1,14 @@
 // components/DoughnutChart.js
 import { Doughnut } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, ChartOptions } from 'chart.js';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const DoughnutChart = ({ data }) => {
-  const options = {
+  const options: ChartOptions<'doughnut'> = {
     plugins: {
       legend: {
-        position: 'bottom',
+        position: 'bottom', // Utilisez une des valeurs acceptées
         labels: {
           boxWidth: 20,
           boxHeight: 20,
